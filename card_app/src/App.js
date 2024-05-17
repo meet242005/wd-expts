51181e486642789d4f05f9a7829f1896034eb8f5
Meet Chavan <meetchavan24@gmail.com>
2024-05-17 22:23:11 +0530

    created react card

--- card_app/src/App.js
+++ card_app/src/App.js
@@ -1,12 +1,11 @@
 import 'bootstrap';
 import './App.css';
+import image1 from './Waves/Deep Purple.jpg'
 import {React, useState,useEffect} from 'react';
-
 
 function App() {
   
   const [isloaded,loadData] = useState(false);
-  const [description, showDescription] = useState(false)
 
   useEffect(()=>{
     setTimeout(()=>{loadData(true)},3000)
@@ -15,20 +14,30 @@ function App() {
   function hide()
   {
     document.getElementsByTagName('h1')[0].style.visibility = "hidden"
-    showDescription(true)
+
+  }
+  function Card(props){
+    return  (
+    <div class="container">
+      <img src={image1}/>
+      <div class="content">
+          <h1 onClick={()=>hide()}>{props.title}</h1>
+          <hr/>
+          
+        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod aliquam, neque excepturi necessitatibus totam tempora eaque voluptates error minima iste ipsam officia quasi deserunt enim quae itaque voluptatum corrupti cupiditate. Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      
+      </div>
+    </div>)
+  
   }
 
   if (isloaded==true) {
     return (
-      <div class="container">
-          <div class="content">
-              <h1 onClick={()=>hide()}>Blog Post Title</h1>
-              <hr/>
-              
-              {description?
-              (<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod aliquam, neque excepturi necessitatibus totam tempora eaque voluptates error minima iste ipsam officia quasi deserunt enim quae itaque voluptatum corrupti cupiditate. Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga voluptatibus sequi tempora necessitatibus? Modi aut, ipsam necessitatibus porro inventore consequatur animi nihil, deserunt voluptates hic voluptatum adipisci sit? Nobis, eius. Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, ducimus deserunt sit mollitia totam animi et nam ad, minus maiores magni eos, praesentium eaque adipisci dolorum quo temporibus doloremque ea.</p>)
-              :('')}
-          </div>
+      <div class="wrapper">
+      <Card title="sometitle"/>
+      <Card title="sometitle1"/>
+      <Card title="sometitle2"/>
+      <Card title="sometitle4"/>
       </div>
     );
   }

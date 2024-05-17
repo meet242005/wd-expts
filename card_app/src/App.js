3cad67eac748e1acd673139640631ef8bc42d101
Meet Chavan <meetchavan24@gmail.com>
2024-05-17 21:57:54 +0530

    react loader

---
+++ card_app/src/App.js
@@ -0,0 +1,34 @@
+import 'bootstrap';
+import './App.css';
+import {React, useState,useEffect} from 'react';
+
+function App() {
+  
+  const [isloaded,loadData] = useState(false);
+
+  useEffect(()=>{
+    setTimeout(()=>{loadData(true)},3000)
+  },[])
+  if (isloaded==true) {
+    return (
+      <div class="container">
+          <div class="content">
+              <h1>Blog Post Title</h1>
+              <hr/>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod aliquam, neque excepturi necessitatibus totam tempora eaque voluptates error minima iste ipsam officia quasi deserunt enim quae itaque voluptatum corrupti cupiditate. Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga voluptatibus sequi tempora necessitatibus? Modi aut, ipsam necessitatibus porro inventore consequatur animi nihil, deserunt voluptates hic voluptatum adipisci sit? Nobis, eius. Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, ducimus deserunt sit mollitia totam animi et nam ad, minus maiores magni eos, praesentium eaque adipisci dolorum quo temporibus doloremque ea.</p>
+          </div>
+      </div>
+    );
+  }
+  else{
+  return (
+    <div class="container">   
+        <div class="loader">
+          
+        </div>
+    </div>
+  );
+}
+}
+
+export default App;
